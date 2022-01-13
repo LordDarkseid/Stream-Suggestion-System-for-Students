4a57ab65d283027594ab586a57a609281d99688b
Mohammed Sohaib Rasul <63374066+LordDarkseid@users.noreply.github.com>
2022-01-13 18:15:01 +0530

    Update StreamSuggest.pl

--- StreamSuggest.pl
+++ StreamSuggest.pl
@@ -1,23 +1,3 @@
-/*
-Stream Suggestion System 
-Made By: Mohammed Sohaib Rasul
-
-Given the interests of a student this program will help suggest suitable streams and subjects for students going to class 11
-
-Just enter the name of the student and the stream you want to check in the format study(student_name, stream). if you want to check if that stream is suitable.
-Or enter study(student_name, X). this will return the streams which are suitable from the pool, type a semicolon to see if more than 1 stream is suitable.
-The program will return Yes or No or the name of the stream based on the subjects of interest of the student.
-
-Subject pool->
-non_med[physics, chemistry, math, computer science].
-med[physics, chemistry, biology, biotechnology,].
-commerce_withoutmath[accounts, business studies, economics, ,legal studies].
-commerce_withmath[accounts, business studies, economics, math].
-humanities_withoutmath[political science, geography, history, psychology].
-humanities_withmath[political science, geography, history, math].
-pcmb[physics, chemistry, math, biology].
-*/
-
 %non med
 likes(sudhir, math).
 likes(sudhir, physics).
